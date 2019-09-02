9d63b9003ddbfd9ef20288d2296ec23ceea2f19c
zhuzi51 <qianlicao51>
2019-09-03 07:26:24 +0800

    akkadb

--- src/main/scala/actor/char01/AkkaDemyDb.scala
+++ src/main/scala/actor/char01/AkkaDemyDb.scala
@@ -1,6 +1,6 @@
 package actor.char01
 
-import akka.actor.{Actor, ActorSystem, Props, Status}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Status}
 import akka.event.Logging
 
 import scala.collection.mutable
@@ -10,25 +10,29 @@ class AkkaDemyDb extends Actor {
   val map = new mutable.HashMap[String, Object]()
   val log = Logging(context.system, this)
 
-override def receive: Receive = {
-  case SetRequest(key, value) => {
-    log.info("received SetRequest - key: {}  value{}", key, value)
-    map.put(key, value)
-  }
-  case GetRequest(key)=>
-    log.info("received GetRequest -key:{}",key)
-    val response:Option[Object]=map.get(key)
-    response match {
-      case Some(x) => sender() !x
-      case None => sender() ! Status.Failure(new KeyNotFoundException(s"找不到此${key}"))
+  override def receive: Receive = {
+    case SetRequest(key, value) => {
+      log.info("received SetRequest - key: {}  value{}", key, value)
+      map.put(key, value)
     }
-  case ex => log.info("received unknown message :{}", ex)
-}
+    case GetRequest(key) =>
+      log.info("received GetRequest -key:{}", key)
+      log.info(map.toString())
+      val response: Option[Object] = map.get(key)
+      response match {
+        case Some(x) => {
+          log.info(s"result {}", x)
+          sender() ! x
+        }
+        case None => sender() ! Status.Failure(new KeyNotFoundException(s"找不到此${key}"))
+      }
+    case ex => log.info("received unknown message :{}", ex)
+  }
 }
 
 
-object Main extends App{
-  val system= ActorSystem("akkademy")
-  system.actorOf(Props[AkkaDemyDb],name = "akkademy-db")
+object Main extends App {
+  val system = ActorSystem("akkademy")
+  private val akkademyDB: ActorRef = system.actorOf(Props[AkkaDemyDb], name = "akkademyDB")
 
 }
--- src/main/scala/actor/char01/SClinet.scala
+++ src/main/scala/actor/char01/SClinet.scala
@@ -1,46 +1,71 @@
-package actor.char01
-
-import akka.actor.ActorSystem
+import actor.char01.{GetRequest, SetRequest}
+import akka.actor.{Actor, ActorRef, ActorSelection, ActorSystem, Props}
+import akka.event.Logging
 import akka.pattern.ask
 import akka.util.Timeout
-import org.scalatest.{FunSpecLike, Matchers}
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class SClinet(remoteAdress: String) {
+class SClinet(remoteAddress: String, port: Int) extends Actor {
   private implicit val timeout = Timeout(2 seconds)
-  private implicit val system = ActorSystem("localSystem")
-  private val remoteDb = system.actorSelection(s"akka.tcp://akkademy@127.0.0.1:2552")
+  private implicit val system = ActorSystem("LocalSystem")
+  val log = Logging(context.system, this)
+  var remoteDb: ActorSelection = _
+
+  //actor 中有个方法 preStart方法，会在actor运行前执行|初始化 工作
+  override def preStart(): Unit = {
+    remoteDb = context.actorSelection(s"akka.tcp://akkademy@${remoteAddress}:${port}/user/akkademyDB")
+    println("客户端初始化{} 远程地址{}", remoteDb, remoteAddress)
+  }
 
 
   def set(key: String, value: Object) = {
     //    remoteDb ? SetRequest(key,value)
-    remoteDb ? SetRequest(key, value) //?需引入 import akka.pattern.ask
+    log.info("serverdb{}", remoteDb)
+    log.info("receive key{}  value{}", key, value)
+    remoteDb ! SetRequest(key, value) //?需引入 import akka.pattern.ask
   }
 
   def get(key: String) = {
     remoteDb ? GetRequest(key)
   }
+
+  override def receive: Receive = {
+    case SetRequest(key, value) => {
+      remoteDb ! SetRequest(key, value)
+    }
+    case GetRequest(key) => {
+      remoteDb ? GetRequest(key)
+      context.stop(self) //停止当前 actorref |
+      context.system.terminate() //退出 sctorSystem
+    }
+  }
 }
 
-/**
- * 测试用例
- *
- */
- class SClientIntegrationSpec extends FunSpecLike with Matchers{
-  val client = new SClinet("127.0.0.1:2552")
 
-  describe("akkademyDblient"){
-    it("should set a value"){
-      client.set("123",new Integer(123))
-      val futureResult= client.get("123")
-      val result=Await.result(futureResult,10 seconds)
+object SClinet extends App {
 
-      result should equal(123)
+  val (clienthost, clientport, serverHost, serverPort) = ("127.0.0.1", 9990, "127.0.0.1", 9989)
+  val config = ConfigFactory.parseString(
+    s"""
+       |akka.actor.provider=akka.remote.RemoteActorRefProvider
+       |akka.remote.netty.tcp.hostnam=$clienthost
+       |akka.remote.netty.tcp.port=$clientport
+       |""".stripMargin)
+  //创建actor|名字很重要
+  val actorSystem = ActorSystem("client", config)
 
-    }
+  implicit val timeout = Timeout(5 seconds)
+  // acotr 和ActorRef
+  val sclientActorRef: ActorRef = actorSystem.actorOf(Props(new SClinet("127.0.0.1", 9989)), "clientActorRef")
+  sclientActorRef ! SetRequest("124", new Integer(123))
 
-  }
 
+  val future = sclientActorRef ? GetRequest("123")
+  val result = Await.result(future.mapTo[Object], 10 seconds)
+  println(s"result:${result}")
+
+  // 启动actor
 }
